7b4031a822d2e9ba9a541157f35f6d38b4a76f1b
Ola Okelola <ola@okelola.com>
2020-12-12 22:45:48 -0800

    day 13 part 2

--- day13.go
+++ day13.go
@@ -1,70 +1,77 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/big"
 	"strings"
 )
 
 type datapoint struct {
-	id int
-	t  int
+	id  int
+	mod int
+}
+
+var one = big.NewInt(1)
+
+// from
+// https://rosettacode.org/wiki/Chinese_remainder_theorem#Go
+func crt(a, n []*big.Int) (*big.Int, error) {
+	p := new(big.Int).Set(n[0])
+	for _, n1 := range n[1:] {
+		p.Mul(p, n1)
+	}
+	var x, q, s, z big.Int
+	for i, n1 := range n {
+		q.Div(p, n1)
+		z.GCD(nil, &s, n1, &q)
+		if z.Cmp(one) != 0 {
+			return nil, fmt.Errorf("%d not coprime", n1)
+		}
+		x.Add(&x, s.Mul(a[i], s.Mul(&s, &q)))
+	}
+	return x.Mod(&x, p), nil
 }
 
 func day13() {
 	lines := readFile("day13input")
-	timestamp := atoi(lines[0])
 
 	parts := strings.Split(lines[1], ",")
 	var data []datapoint
+	var a []*big.Int
+	var n []*big.Int
 	for i, part := range parts {
 		if part == "x" {
 			continue
 		}
 
 		data = append(data, datapoint{
-			id: atoi(part),
-			t:  i,
+			id:  atoi(part),
+			mod: i,
 		})
+		num := atoi(part)
+		a = append(a, big.NewInt(int64(num-i)))
+		n = append(n, big.NewInt(int64(num)))
 	}
 
-	first := data[0]
-	ts := first.id
+	// previous solution didn't work so have to use math
+	// apparently, there's a CRT solution
+	// crt solution
+	log.Println(crt(a, n))
 
-	// find minimum value
-	var earliest int
+	// solution:	825305207525452
+	// and math below...
 
-	idx := 0
-	for i := idx; true; i++ {
-		if ts*i > timestamp {
-			earliest = ts * i
-			break
+	minValue := 0
+	runningProduct := 1
+	for _, v := range data {
+		for (minValue+v.mod)%v.id != 0 {
+			minValue += runningProduct
 		}
+		runningProduct *= v.id
 	}
+	log.Println(minValue)
 
-	for {
-		correct := true
-		for _, datum := range data {
-			if (earliest+datum.t)%datum.id != 0 {
-				correct = false
-				break
-			}
-		}
-		if correct {
-			log.Println(earliest)
-			break
-		}
-		idx++
-		earliest = ts * idx
-	}
-
-	//		idx++
-	//	}
-	//		}
-	// sort.Slice(earliest, func(i, j int) bool {
-	// 	return earliest[i][0] < earliest[j][0]
-	// })
-	// //	log.Println(ids)
-	// //	log.Println(earliest)
-	// log.Println((earliest[0][0] - timestamp) * earliest[0][1])
-
+	// and another explanation from @astonm
+	// https://github.com/astonm/advent-of-code-2020/blob/main/day13/code.py#L35-L50
 }
