756b695501e508d663e7149f281291be8d31131b
Ola Okelola <ola@okelola.com>
2020-12-13 22:25:00 -0800

    day 14 part 2

--- day14.go
+++ day14.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"math"
+	"strconv"
 	"strings"
 )
 
@@ -15,60 +16,83 @@ func day14() {
 		if strings.HasPrefix(line, "mask") {
 			parts := splitLength(line, "=", 2)
 			mask = strings.TrimSpace(parts[1])
-			log.Println(mask)
+			//			log.Println(mask)
 		} else {
 			parts := splitLength(line, "=", 2)
 			decimal := atoi(strings.TrimSpace(parts[1]))
 			idx := strings.Index(parts[0], "]")
 			address := atoi(strings.TrimSpace(parts[0][4:idx]))
-			//			log.Println("address", address)
-			result := 0
+
+			result := make(map[int]byte, 36)
+			var floating []int
 
 			for i := 35; i >= 0; i-- {
 				pow := int(math.Pow(2, float64(i)))
-				switch mask[35-i] {
+				newIdx := 35 - i
+
+				switch mask[newIdx] {
 				case 'X':
-					// leave bit unchanged
-					//					log.Println("idx", i)
-					if pow <= decimal {
-						result |= pow
-						decimal = decimal - pow
-					}
+					// floating
+					result[newIdx] = 'X'
+					floating = append(floating, newIdx)
+
 					break
 				case '1':
 					// mask and change the bit
-					result |= pow
-					if pow <= decimal {
-						decimal = decimal - pow
-					}
+					result[newIdx] = 1
 					break
 				case '0':
-					if pow <= decimal {
-						decimal = decimal - pow
+					// leave bit unchanged
+					if pow <= address {
+						// bit is set
+						result[newIdx] = 1
 					}
 					break
 				}
-				// //				if mask[35-i] == ''
-				// if mask[35-i] == '1' {
-				// 	log.Println("mask ", i)
-				// 	result |= pow
-				// 	decimal = decimal - pow
-				// 	continue
-				// }
-				// if pow > decimal {
-				// 	continue
-				// }
-				// if mask[i] == '0' {
-				// 	continue
-				// }
-				// result |= pow
-				// log.Println("idx", i)
-				// decimal = decimal - pow
-				// log.Println("result", result)
+				if pow <= address {
+					address = address - pow
+				}
 			}
-			if result != 0 {
-				m[address] = result
+
+			numFloating := int(math.Pow(2, float64(len(floating))))
+
+			// log.Println("floating", floating)
+			// log.Println("num", numFloating)
+			// log.Println("result", result)
+			for i := 0; i < numFloating; i++ {
+				str := strconv.FormatInt(int64(i), 2)
+				if len(str) < len(floating) {
+					var sb strings.Builder
+					for j := 0; j < len(floating)-len(str); j++ {
+						sb.WriteString("0")
+					}
+					sb.WriteString(str)
+					str = sb.String()
+				}
+				result2 := make(map[int]byte)
+				for k, v := range result {
+					result2[k] = v
+				}
+				//				log.Println("str", str)
+				//				log.Println("floating", floating, str)
+				for j := 0; j < len(floating); j++ {
+					if str[j] == '1' {
+						result2[floating[j]] = 1
+					}
+				}
+				//				log.Println("result2", result2)
+				number := 0
+				for k, v := range result2 {
+					if v == 1 {
+						number += int(math.Pow(2, float64(35-k)))
+					}
+				}
+				//				log.Println("number", number)
+				if number != 0 {
+					m[number] = decimal
+				}
 			}
+
 		}
 	}
 
