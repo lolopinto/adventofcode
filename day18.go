1df5afc2775a4c08b6ab1b086acca88b4a9f81ad
Ola Okelola <ola@okelola.com>
2020-12-18 01:02:33 -0800

    day 18 part 2
    LOLOL
    
    did all that complicated nonsense then when faced with operator precedence did this the correct way

--- day18.go
+++ day18.go
@@ -1,171 +1,78 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math"
 	"strings"
-	"unicode"
 )
 
-type part struct {
-	line  string
-	parts []*part
-}
-
-func (p *part) calculate(prev int) int {
-	if p.line == ")" {
-		return prev
-	}
-	if len(p.parts) == 0 && p.line == "" {
-		return prev
-	}
-	if len(p.parts) == 0 {
-		return calculateLine(p.line, prev)
-	}
-	// has parts use that
-	last := 0
-	result := 0
-	for i := 0; i < len(p.parts); i++ {
-		p2 := p.parts[i]
-		// starts with a digit
-		// do in isolation
-		if unicode.IsDigit(rune(p2.line[0])) {
-			//			last = calculateLine(p2.line, 0)
-			result = p2.calculate(last)
-		} else if p2.line == "*" {
-			// look ahead...
-			//			result = last * calculateLine(p.parts[i+1].line, 1)
-			result = result * p.parts[i+1].calculate(1)
-			i++
-		} else if p2.line == "+" {
-			//			log.Println("last", last)
-			// look ahead...
-			//			result = last + calculateLine(p.parts[i+1].line, 1)
-			result = result + p.parts[i+1].calculate(1)
-			i++
-		} else { // starts with a + or something
-			//			result = calculateLine(p2.line, result)
-			result = p2.calculate(result)
+func findFirstPlus(ops []string) int {
+	for i := 0; i < len(ops); i++ {
+		op := ops[i]
+		if op == "+" {
+			return i
 		}
 	}
-	return result
+	return -1
 }
 
-func calculateLine(line string, result int) int {
-	ops := strings.Split(strings.TrimSpace(line), " ")
-	var lastOp string
-	for _, op := range ops {
+func findInnerBrack(line string) *[2]int {
+	left := []int{}
 
-		switch op {
-		case "*":
-			lastOp = "*"
-			break
-		case "+":
-			lastOp = "+"
-			break
-		default:
-			if lastOp == "" {
-				result = atoi(op)
-			} else if lastOp == "*" {
-				if result == 0 {
-					result = 1 * atoi(op)
-					lastOp = ""
-				} else {
-					result = result * atoi(op)
-					lastOp = ""
-				}
-			} else if lastOp == "+" {
-				//				log.Println(op, "..", line)
-				result += atoi(op)
-				lastOp = ""
-			}
-		}
-	}
-	return result
-}
-
-func breakParts(line string) []*part {
-	var results []*part
-	var left []int
-	var right []int
-
-	var found bool
-	lastLeft := 0
-	lastRight := 0
 	for i, c := range line {
 		if c == '(' {
 			left = append(left, i)
-			found = true
 		}
 		if c == ')' {
-			right = append(right, i)
-			found = true
-		}
-		if len(left) > 0 && len(right) == len(left) {
-
-			lastLeft = left[0]
-			// everything before left
-			if lastLeft != lastRight {
-				results = append(results, &part{line: line[lastRight:lastLeft]})
-			}
-			results = append(results, &part{line: line[lastLeft:left[0]]})
-
-			//			log.Println("match", line, left, right)
-			idx := len(left) - 1
-			p := &part{
-				line: line[left[0]+1 : right[idx]],
-			}
-			if right[idx] != len(line) {
-				// break into parts
-				p.parts = breakParts(line[left[0]+1 : right[idx]])
-			}
-
-			results = append(results, p)
-
-			lastRight = right[idx]
-			left = []int{}
-			right = []int{}
+			// found a matching pair!
+			return &[2]int{left[len(left)-1], i}
 		}
 	}
-	if found {
-		if lastRight != 0 {
-			results = append(results, &part{line: line[lastRight+1:]})
-		}
-	} else {
-		results = append(results, &part{line: line})
-	}
-
-	// cleanup...
-	var ret []*part
-	for _, r := range results {
-		//		log.Println(r.line)
-		r.line = strings.TrimSpace(strings.TrimPrefix(r.line, ")"))
-		//		log.Println(r.line)
-		if r.line == "" {
-			continue
-		}
-		last := len(r.line) - 1
-		//		log.Println(r.line, len(r.line))
-
-		if (r.line[last] == '+' || r.line[last] == '*') && len(r.line) != 1 {
-			// if !unicode.IsDigit(rune(r.line[last])) {
-			old := r.line
-			r.line = r.line[0 : last-1]
-			ret = append(ret, r)
-			ret = append(ret, &part{line: old[last:]})
-		} else {
-			ret = append(ret, r)
-		}
-	}
-	return ret
+	return nil
 }
 
-func parseArith(line string) int {
+func calculateLine(line string) int {
+	for {
+		brack := findInnerBrack(line)
+		if brack == nil {
+			break
+		}
 
-	parts := breakParts(line)
-	//	spew.Dump(len(parts), parts)
-	p := &part{parts: parts}
+		sub := line[brack[0]+1 : brack[1]]
+		val := calculateLine(sub)
+		line = strings.Replace(line, line[brack[0]:brack[1]+1], fmt.Sprintf("%d", val), 1)
+	}
+	ops := strings.Split(strings.TrimSpace(line), " ")
 
-	return p.calculate(0)
+	for {
+		idx := findFirstPlus(ops)
+		if idx == -1 {
+			break
+		}
+		sum := atoi(ops[idx-1]) + atoi(ops[idx+1])
+		var ops2 []string
+		var added bool
+		for i, v := range ops {
+			if math.Abs(float64(idx-i)) <= 1 {
+				if !added {
+					ops2 = append(ops2, fmt.Sprintf("%d", sum))
+				}
+				added = true
+			} else {
+				ops2 = append(ops2, v)
+			}
+		}
+		ops = ops2
+	}
+	mult := 1
+	for _, v := range ops {
+		if v != "*" {
+			mult *= atoi(v)
+		}
+	}
+	return mult
+
 }
 
 func day18() {
@@ -173,14 +80,8 @@ func day18() {
 
 	sum := 0
 	for _, line := range lines {
-
-		// parts := strings.Split(line, " ")
-		// spew.Dump(parts)
-		//		log.Println(line)
-		ans := parseArith(line)
-		//		log.Println(line, ans)
+		ans := calculateLine(line)
 		sum += ans
 	}
 	log.Println(sum)
-
 }
