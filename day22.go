1a456709c089fef99f9d659e1f4cc9d39cb8aaee
Ola Okelola <ola@okelola.com>
2020-12-21 22:10:40 -0800

    day 22 part 2

--- day22.go
+++ day22.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/davecgh/go-spew/spew"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+)
 
 func day22() {
 	chunks := readFileChunks("day22input", 2)
@@ -8,30 +13,131 @@ func day22() {
 	p1 := parsePlayer(chunks[0])
 	p2 := parsePlayer(chunks[1])
 
-	// spew.Dump(p1)
-	// spew.Dump(p2)
+	globalNum = 1
+	g := &game{num: globalNum}
+	winner := g.playGame(p1, p2)
 
+	spew.Dump(winner.calcScore())
+}
+
+type game struct {
+	player1Winner bool
+	num           int
+}
+
+var globalNum int
+
+func (g *game) playGame(p1, p2 *player) *player {
+	round := 1
 	for {
-		if len(p1.cards) == 0 || len(p2.cards) == 0 {
+		//		log.Println("game", g.num, "round", round, p1.cards, p2.cards)
+
+		if g.gameOver(p1, p2) {
+			//			log.Println("game over", round)
 			break
 		}
-
 		c1 := p1.pop()
 		c2 := p2.pop()
 
-		if c1 > c2 {
-			p1.append(c1, c2)
-		} else {
-			p2.append(c1, c2)
-		}
+		g.calcWinner(p1, p2, c1, c2)
+
+		round++
+	}
+	if g.player1Winner {
+		return p1
+	}
+	if len(p1.cards) == 0 {
+		return p2
+	}
+	return p1
+}
+
+func (g *game) cloneAndPlay(p1, p2 *player, c1, c2 int) *player {
+	//	log.Println(c1, c2)
+	p1cards := make([]int, c1)
+	for i := 0; i < c1; i++ {
+		//	for k, v := range p1.cards {
+		p1cards[i] = p1.cards[i]
+	}
+	p2cards := make([]int, c2)
+	for i := 0; i < c2; i++ {
+		p2cards[i] = p2.cards[i]
 	}
 
-	spew.Dump(p1.calcScore())
-	spew.Dump(p2.calcScore())
+	globalNum++
+	g2 := &game{num: globalNum}
+
+	p1clone := &player{cards: p1cards}
+	p2clone := &player{cards: p2cards}
+	winner := g2.playGame(p1clone, p2clone)
+
+	if winner == p1clone {
+		return p1
+	}
+	return p2
+}
+
+func (g *game) calcWinner(p1, p2 *player, c1, c2 int) *player {
+	if len(p1.cards) >= c1 && len(p2.cards) >= c2 {
+		winner := g.cloneAndPlay(p1, p2, c1, c2)
+
+		// win via clone, swap the lower number first
+		if winner == p1 {
+			winner.append(c1, c2)
+		} else {
+			winner.append(c2, c1)
+		}
+
+		return nil
+	}
+
+	if g.player1Winner {
+		//		return p1
+	}
+	if c1 > c2 {
+		p1.append(c1, c2)
+	} else {
+		p2.append(c2, c1)
+	}
+	// if c1 > c2 {
+	// 	return p1
+	// }
+	// return p2
+	return nil
+}
+
+func (g *game) gameOver(p1, p2 *player) bool {
+	if p1.previouslySeen() || p2.previouslySeen() {
+		g.player1Winner = true
+		return true
+	}
+	return len(p1.cards) == 0 || len(p2.cards) == 0
 }
 
 type player struct {
-	cards []int
+	cards      []int
+	prevRounds map[string]int
+}
+
+func (p *player) getKey() string {
+	strs := make([]string, len(p.cards))
+	for i, v := range p.cards {
+		strs[i] = fmt.Sprintf("%v", v)
+	}
+	return strings.Join(strs, ",")
+}
+
+func (p *player) calcPreviousRound() {
+	if p.prevRounds == nil {
+		p.prevRounds = make(map[string]int)
+	}
+	key := p.getKey()
+	val, ok := p.prevRounds[key]
+	if !ok {
+		p.prevRounds[key] = 1
+	} else {
+		p.prevRounds[key] = val + 1
+	}
 }
 
 func (p *player) pop() int {
@@ -41,13 +147,13 @@ func (p *player) pop() int {
 }
 
 func (p *player) append(c1, c2 int) {
-	if c1 > c2 {
-		p.cards = append(p.cards, c1)
-		p.cards = append(p.cards, c2)
-	} else {
-		p.cards = append(p.cards, c2)
-		p.cards = append(p.cards, c1)
-	}
+	p.cards = append(p.cards, c1, c2)
+	p.calcPreviousRound()
+}
+
+func (p *player) previouslySeen() bool {
+	key := p.getKey()
+	return p.prevRounds[key] == 2
 }
 
 func (p *player) calcScore() int {
