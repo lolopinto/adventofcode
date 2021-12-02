9e5cf82aaa822168aad4da05e58d6d11195931fe
Ola Okelola <ola@okelola.com>
2021-12-02 00:05:51 -0500

    day 2 part 2

--- day2.go
+++ day2.go
@@ -4,6 +4,29 @@ import "fmt"
 
 func day2() {
 	horizontal := 0
+	aim := 0
+	depth := 0
+	lines := readFile("day2input")
+	for _, line := range lines {
+		parts := splitLength(line, " ", 2)
+		dir := parts[0]
+		num := atoi(parts[1])
+
+		switch dir {
+		case "forward":
+			horizontal += num
+			depth += (aim * num)
+		case "down":
+			aim += num
+		case "up":
+			aim -= num
+		}
+	}
+	fmt.Println(horizontal * depth)
+}
+
+func day2part1() {
+	horizontal := 0
 	depth := 0
 	lines := readFile("day2input")
 	for _, line := range lines {
@@ -21,8 +44,4 @@ func day2() {
 		}
 	}
 	fmt.Println(horizontal * depth)
-}
-
-func day2part1() {
-
 }
