82a36c0d26337cc66d992c5a156d0b09fece56c4
Ola Okelola <ola@okelola.com>
2021-12-07 09:51:27 -0500

    remove map

--- day7.go
+++ day7.go
@@ -10,19 +10,15 @@ func day7() {
 	lines := readFile("day7input")
 	input := ints(strings.Split(lines[0], ","))
 
-	m := make(map[int]int)
+	least := math.MaxInt
 	for i := range input {
 		sum := 0
 		for j := range input {
 			diff := int(math.Abs(float64(input[j] - i)))
 			sum += ((diff * (diff + 1)) / 2)
 		}
-		m[i] = sum
-	}
-	least := math.MaxInt
-	for _, v := range m {
-		if v < least {
-			least = v
+		if sum < least {
+			least = sum
 		}
 	}
 	fmt.Println(least)
