9164590f0a0267685c7de6817fa362c725802eca
Ola Okelola <ola@okelola.com>
2021-12-09 21:25:23 -0800

    day 10 part 2

--- day10.go
+++ day10.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"sort"
 )
 
 func day10() {
 	lines := readFile("day10input")
-	sum := 0
+	var incomplete []int
 	for _, line := range lines {
-		sum += parseLine(line)
+		inc := parseLine(line)
+		if inc != 0 {
+			incomplete = append(incomplete, inc)
+		}
 	}
-	fmt.Println(sum)
+	fmt.Println(incomplete)
+	sort.Ints(incomplete)
+	idx := int(math.Ceil(float64(len(incomplete) / 2)))
+	fmt.Println(incomplete[idx])
 }
 
 var closing = map[rune]rune{
@@ -20,6 +28,7 @@ var closing = map[rune]rune{
 	'<': '>',
 }
 
+// used in part1
 var invalidpoints = map[rune]int{
 	')': 3,
 	']': 57,
@@ -27,8 +36,14 @@ var invalidpoints = map[rune]int{
 	'>': 25137,
 }
 
+var incompletepoints = map[rune]int{
+	')': 1,
+	']': 2,
+	'}': 3,
+	'>': 4,
+}
+
 func parseLine(s string) int {
-	sum := 0
 	var list []rune
 	for _, c := range s {
 		switch c {
@@ -39,11 +54,24 @@ func parseLine(s string) int {
 			last := list[len(list)-1]
 			// valid
 			if closing[last] != c {
-				sum += invalidpoints[c]
-				//				fmt.Println("invalid line", s)
+				return 0
+				// used in part1
+				//				sum += invalidpoints[c]
 			}
 			list = list[0 : len(list)-1]
 		}
 	}
-	return sum
+
+	// incomplete
+	result := 0
+	var l2 []rune
+	for i := len(list) - 1; i >= 0; i-- {
+		c := list[i]
+		l2 = append(l2, closing[c])
+	}
+	for _, c := range l2 {
+		result = result * 5
+		result = result + incompletepoints[c]
+	}
+	return result
 }
