7b574cf47423505fdd2310e6ca97b0a8baa610e3
Ola Okelola <ola@okelola.com>
2021-12-12 01:20:04 -0800

    remove node class

--- day12.go
+++ day12.go
@@ -7,16 +7,24 @@ import (
 
 func day12() {
 	lines := readFile("day12input")
-	nodes := make(map[string]*node)
-	//	neighbors := make
-	for _, line := range lines {
-		parts := strings.Split(line, "-")
-		parseNode(parts[0], nodes)
-		parseNode(parts[1], nodes)
-		addNeighbors(parts[0], parts[1], nodes)
+	neighbors := make(map[string][]string)
+
+	addNeighbor := func(n1, n2 string) {
+		l, ok := neighbors[n1]
+		if !ok {
+			l = []string{}
+		}
+		l = append(l, n2)
+		neighbors[n1] = l
 	}
 
-	fmt.Println(checkPaths([]string{"start"}, nodes, false))
+	for _, line := range lines {
+		parts := strings.Split(line, "-")
+		addNeighbor(parts[0], parts[1])
+		addNeighbor(parts[1], parts[0])
+	}
+
+	fmt.Println(checkPaths([]string{"start"}, neighbors, false))
 }
 
 func contains(haystack []string, needle string) bool {
@@ -28,7 +36,7 @@ func contains(haystack []string, needle string) bool {
 	return false
 }
 
-func checkPaths(l []string, nodes map[string]*node, visited bool) int {
+func checkPaths(l []string, neighbors map[string][]string, visited bool) int {
 	count := 0
 	lastIdx := len(l) - 1
 	last := l[lastIdx]
@@ -37,39 +45,16 @@ func checkPaths(l []string, nodes map[string]*node, visited bool) int {
 		return 1
 	}
 
-	for _, neighbor := range nodes[last].neighbors {
+	for _, neighbor := range neighbors[last] {
 		// can check for upper or for something not yet visited
-		if strings.ToUpper(neighbor.node) == neighbor.node || !contains(l, neighbor.node) {
-			count += checkPaths(append(l, neighbor.node), nodes, visited)
+		if strings.ToUpper(neighbor) == neighbor || !contains(l, neighbor) {
+			count += checkPaths(append(l, neighbor), neighbors, visited)
 		}
 
-		if !visited && neighbor.node != "start" && neighbor.node != "end" && strings.ToLower(neighbor.node) == neighbor.node && contains(l, neighbor.node) {
-			count += checkPaths(append(l, neighbor.node), nodes, true)
+		if !visited && neighbor != "start" && neighbor != "end" && strings.ToLower(neighbor) == neighbor && contains(l, neighbor) {
+			count += checkPaths(append(l, neighbor), neighbors, true)
 		}
 	}
 
 	return count
-}
-
-type node struct {
-	node      string
-	neighbors []*node
-}
-
-func parseNode(part string, nodes map[string]*node) {
-	if nodes[part] != nil {
-		return
-	}
-
-	n := &node{
-		node: part,
-	}
-	nodes[part] = n
-}
-
-func addNeighbors(part1, part2 string, nodes map[string]*node) {
-	n1 := nodes[part1]
-	n2 := nodes[part2]
-	n1.neighbors = append(n1.neighbors, n2)
-	n2.neighbors = append(n2.neighbors, n1)
 }
