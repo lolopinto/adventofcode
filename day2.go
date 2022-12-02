79dfa4b7f79a39568e803cd1d7bf68a2ef9414e9
Ola Okelola <ola@okelola.com>
2022-12-01 21:20:35 -0800

    part 2

--- day2.go
+++ day2.go
@@ -3,47 +3,77 @@ package main
 import "fmt"
 
 func day2() {
-	opp := map[string]string{"A": "rock", "B": "paper", "C": "scissors"}
-	me := map[string]string{"X": "rock", "Y": "paper", "Z": "scissors"}
+	opp := map[string]string{"A": "R", "B": "P", "C": "S"}
+	// me := map[string]string{"X": "R", "Y": "P", "Z": "S"}
+
 	lines := readFile("day2input")
 	sum := 0
 	for _, line := range lines {
 		parts := splitLength(line, " ", 2)
-		sum += score(opp[parts[0]], me[parts[1]])
+
+		// sum += score(opp[parts[0]], me[parts[1]])
+		sum += score(opp[parts[0]], getMine(opp[parts[0]], parts[1]))
 	}
 	fmt.Println(sum)
+}
 
+func getMine(opp, me string) string {
+	switch opp {
+	case "R":
+		if me == "X" {
+			return "S"
+		} else if me == "Y" {
+			return "R"
+		} else {
+			return "P"
+		}
+	case "P":
+		if me == "X" {
+			return "R"
+		} else if me == "Y" {
+			return "P"
+		} else {
+			return "S"
+		}
+	default:
+		if me == "X" {
+			return "P"
+		} else if me == "Y" {
+			return "S"
+		} else {
+			return "R"
+		}
+	}
 }
 
 func score(opp, me string) int {
 	s := 0
 	switch me {
-	case "rock":
+	case "R":
 		s += 1
-		if opp == "rock" {
+		if opp == "R" {
 			s += 3
 		}
-		if opp == "scissors" {
+		if opp == "S" {
 			s += 6
 		}
 
-	case "paper":
+	case "P":
 		s += 2
-		if opp == "paper" {
+		if opp == "P" {
 			s += 3
 		}
-		if opp == "rock" {
+		if opp == "R" {
 			s += 6
 		}
 	default:
 		s += 3
-		if opp == "scissors" {
+		if opp == "S" {
 			s += 3
 		}
-		if opp == "paper" {
+		if opp == "P" {
 			s += 6
 		}
 	}
 	return s
-
 }
