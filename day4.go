66cce73b66c09ee7d335324be5da1c115eb0756d
Ola Okelola <ola@okelola.com>
2022-12-03 21:15:05 -0800

    day 4 part 2

--- day4.go
+++ day4.go
@@ -15,21 +15,26 @@ func day4() {
 	}
 
 	contains := func(r1, r2 [2]int) bool {
-		if r1[0] >= r2[0] && r1[1] <= r2[1] {
-			return true
-		}
-		return false
+		return r1[0] >= r2[0] && r1[1] <= r2[1]
+	}
+	overlaps := func(r1, r2 [2]int) bool {
+		return r1[0] >= r2[0] && r1[0] <= r2[1]
 	}
 
-	ct := 0
+	ct1 := 0
+	ct2 := 0
 	for _, line := range lines {
 		parts := strings.Split(line, ",")
 		r1 := parseRange(parts[0])
 		r2 := parseRange(parts[1])
 
 		if contains(r1, r2) || contains(r2, r1) {
-			ct += 1
+			ct1 += 1
+		}
+		if overlaps(r1, r2) || overlaps(r2, r1) {
+			ct2 += 1
 		}
 	}
-	fmt.Println(ct)
+	fmt.Println(ct1)
+	fmt.Println(ct2)
 }
