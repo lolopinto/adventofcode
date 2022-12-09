909f32d8a41c2ee27b01c90011002cbcb5b1b553
Ola Okelola <ola@okelola.com>
2022-12-08 23:07:28 -0800

    day 9 cleaned up

--- day9.go
+++ day9.go
@@ -76,5 +76,56 @@ func day9() {
 		}
 	}
 
+	day9part1()
+	fmt.Println(len(tail))
+}
+
+func day9part1() {
+	type point struct {
+		x, y int
+	}
+	lines := readFile("day9input")
+	// head := map[point]bool{}
+	start := point{0, 0}
+	tail := map[point]bool{
+		start: true,
+	}
+
+	headPos := start
+	tailPos := start
+
+	updateTailPos := func(pt point) {
+		tail[pt] = true
+		tailPos = pt
+	}
+
+	for _, line := range lines {
+		parts := splitLength(line, " ", 2)
+		dir := parts[0]
+		for i := 0; i < atoi(parts[1]); i++ {
+			switch dir {
+			case "R":
+				headPos.x++
+			case "U":
+				headPos.y--
+			case "D":
+				headPos.y++
+			case "L":
+				headPos.x--
+			}
+
+			diff := point{tailPos.x - headPos.x, tailPos.y - headPos.y}
+
+			if diff.x == 2 {
+				updateTailPos(point{x: headPos.x + 1, y: headPos.y})
+			} else if diff.x == -2 {
+				updateTailPos(point{x: headPos.x - 1, y: headPos.y})
+			} else if diff.y == 2 {
+				updateTailPos(point{x: headPos.x, y: headPos.y + 1})
+			} else if diff.y == -2 {
+				updateTailPos(point{x: headPos.x, y: headPos.y - 1})
+			}
+		}
+	}
 	fmt.Println(len(tail))
 }
