910eddd337cfaa0c88d8ff1441b483f52193bda0
Ola Okelola <ola@okelola.com>
2022-12-09 21:33:58 -0800

    day 10 part 2

--- day10.go
+++ day10.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"strings"
+
+	"github.com/lolopinto/adventofcode2020/grid"
 )
 
 func day10() {
@@ -10,13 +12,30 @@ func day10() {
 	x := 1
 	cycle := 0
 	sum := 0
+
+	// grid dimensions? way grid class is structured is wrong
+	g := grid.NewRectGrid(6, 40)
+
 	increaseCycle := func() {
 		cycle++
 		if cycle == 20 || (cycle-20)%40 == 0 {
-			// fmt.Println(cycle, x)
 			sum += (cycle * x)
 		}
+		// part 2
+		pos := cycle - 1
+		// took too long to understand what they were asking
+		// i hate the wording of these things
+		sprite := []int{x - 1, x, x + 1}
+		cyclewidth := (pos % 40)
+		cycleheight := (pos / 40)
+
+		for _, v := range sprite {
+			if v == cyclewidth {
+				g.At(cycleheight, cyclewidth).SetValue("#")
+			}
+		}
 	}
+
 	for _, line := range lines {
 		if strings.HasPrefix(line, "addx") {
 			increaseCycle()
@@ -27,5 +46,14 @@ func day10() {
 			increaseCycle()
 		}
 	}
+
 	fmt.Println(sum)
+
+	// EALGULPG
+	g.Print(func(val interface{}) string {
+		if val == nil {
+			return "."
+		}
+		return "#"
+	})
 }
