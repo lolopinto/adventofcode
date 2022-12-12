10ecddec2160b715cddeffb398390bd19b1301dc
Ola Okelola <ola@okelola.com>
2022-12-12 09:17:51 -0800

    int64 instead of big.Int

--- day11.go
+++ day11.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/big"
 	"regexp"
 	"sort"
 	"strings"
@@ -10,7 +9,7 @@ import (
 
 type monkey struct {
 	x            int
-	starting     []*big.Int
+	starting     []int64
 	op           string
 	divsibleBy   int
 	throwToTrue  int
@@ -22,26 +21,26 @@ func (m *monkey) addCount(v int) {
 	m.ct += v
 }
 
-func (m *monkey) evaluateOp(left *big.Int) *big.Int {
+func (m *monkey) evaluateOp(left int64) int64 {
 	op := splitLength(m.op, " = ", 2)[1]
 	parts := strings.Split(op, " ")
 	if len(parts) != 3 {
 		panic(fmt.Sprintf("rhs of op %s is not as expected", op))
 	}
 
-	var right *big.Int
+	var right int64
 	if parts[2] == "old" {
 		right = left
 	} else {
-		right = big.NewInt(atoi64(parts[2]))
+		right = atoi64(parts[2])
 	}
 	switch parts[1] {
 
 	case "*":
-		return left.Mul(left, right)
+		return left * right
 
 	case "+":
-		return left.Add(left, right)
+		return left + right
 	}
 
 	panic(fmt.Sprintf("invalid op %s in %s", parts[1], m.op))
@@ -70,10 +69,10 @@ func day11() {
 
 		x := atoi(monkeyMatch[1])
 
-		var starting []*big.Int
+		var starting []int64
 		startingStr := strings.Split(chunk[1], ":")[1]
 		for _, v := range strings.Split(startingStr, ",") {
-			starting = append(starting, big.NewInt(atoi64(strings.TrimSpace(v))))
+			starting = append(starting, atoi64(strings.TrimSpace(v)))
 		}
 
 		opStr := strings.TrimSpace(strings.Split(chunk[2], ":")[1])
@@ -105,27 +104,18 @@ func day11() {
 		product *= atoi(divisbleMatch[1])
 	}
 
-	prodInt := big.NewInt(int64(product))
-	// threeInt := big.NewInt(3)
-
 	for i := 1; i <= 10000; i++ {
 		for i := 0; i < len(chunks); i++ {
 			mon := m[i]
 			for _, s := range mon.starting {
 				mon.addCount(1)
-				// how to evaluate the right number if we don't do the real math if this is "*"
-				v := mon.evaluateOp(s)
-				div := big.NewInt(0)
-
 				// part 1
-				// div.Div(v, threeInt)
+				// v := mon.evaluateOp(s) / 3
 				// part 2
-				div.Mod(v, prodInt)
+				v := mon.evaluateOp(s) % int64(product)
 
 				var mon2 *monkey
-				mod := big.NewInt(0)
-				mod.Mod(div, big.NewInt(int64(mon.divsibleBy)))
-				if mod.IsInt64() && mod.Int64() == 0 {
+				if v%int64(mon.divsibleBy) == 0 {
 					mon2 = m[mon.throwToTrue]
 					if mon2 == nil {
 						panic(fmt.Sprintf("could not find monkey %d", mon.throwToTrue))
@@ -137,11 +127,10 @@ func day11() {
 					}
 				}
 
-				mon2.starting = append(mon2.starting, div)
+				mon2.starting = append(mon2.starting, v)
 			}
-			mon.starting = []*big.Int{}
+			mon.starting = []int64{}
 		}
-
 	}
 
 	var cts []int
