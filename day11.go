79663c82d775ad4a642cd171ef699fabd62b06cd
Ola Okelola <ola@okelola.com>
2022-12-11 18:32:42 -0800

    day 11 part 2

--- day11.go
+++ day11.go
@@ -9,7 +9,7 @@ import (
 
 type monkey struct {
 	x            int
-	starting     []int
+	starting     []int64
 	op           string
 	divsibleBy   int
 	throwToTrue  int
@@ -21,18 +21,18 @@ func (m *monkey) addCount(v int) {
 	m.ct += v
 }
 
-func (m *monkey) evaluateOp(left int) int {
+func (m *monkey) evaluateOp(left int64) int64 {
 	op := splitLength(m.op, " = ", 2)[1]
 	parts := strings.Split(op, " ")
 	if len(parts) != 3 {
 		panic(fmt.Sprintf("rhs of op %s is not as expected", op))
 	}
 
-	var right int
+	var right int64
 	if parts[2] == "old" {
 		right = left
 	} else {
-		right = atoi(parts[2])
+		right = atoi64(parts[2])
 	}
 	switch parts[1] {
 
@@ -56,6 +56,7 @@ func day11() {
 	testTrue := regexp.MustCompile(`If true: throw to monkey (\d+)`)
 	testFalse := regexp.MustCompile(`If false: throw to monkey (\d+)`)
 
+	product := 1
 	for _, chunk := range chunks {
 		if len(chunk) != 6 {
 			panic(fmt.Sprintf("invalid chunk %s", chunk))
@@ -68,10 +69,10 @@ func day11() {
 
 		x := atoi(monkeyMatch[1])
 
-		var starting []int
+		var starting []int64
 		startingStr := strings.Split(chunk[1], ":")[1]
 		for _, v := range strings.Split(startingStr, ",") {
-			starting = append(starting, atoi(strings.TrimSpace(v)))
+			starting = append(starting, atoi64(strings.TrimSpace(v)))
 		}
 
 		opStr := strings.TrimSpace(strings.Split(chunk[2], ":")[1])
@@ -100,22 +101,25 @@ func day11() {
 			throwToTrue:  atoi(testTrueMatch[1]),
 			throwToFalse: atoi(testFalseMatch[1]),
 		}
+		product *= atoi(divisbleMatch[1])
 	}
 
-	for i := 1; i <= 20; i++ {
+	for i := 1; i <= 10000; i++ {
 		for i := 0; i < len(chunks); i++ {
 			mon := m[i]
 			for _, s := range mon.starting {
 				mon.addCount(1)
-				v := mon.evaluateOp(s)
-				v = v / 3
+				// part 1
+				// v := mon.evaluateOp(s) / 3
+				// part 2
+				v := mon.evaluateOp(s) % int64(product)
+
 				var mon2 *monkey
-				if v%mon.divsibleBy == 0 {
+				if v%int64(mon.divsibleBy) == 0 {
 					mon2 = m[mon.throwToTrue]
 					if mon2 == nil {
 						panic(fmt.Sprintf("could not find monkey %d", mon.throwToTrue))
 					}
-					// fmt.Printf("throwing %d to monkey %d. true path\n", v, mon.throwToTrue)
 				} else {
 					mon2 = m[mon.throwToFalse]
 					if mon2 == nil {
@@ -125,9 +129,8 @@ func day11() {
 
 				mon2.starting = append(mon2.starting, v)
 			}
-			mon.starting = []int{}
+			mon.starting = []int64{}
 		}
-
 	}
 
 	var cts []int
