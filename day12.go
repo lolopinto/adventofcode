f1a1596d0ea8ddd0ab2829a736119fd82d5491fa
Ola Okelola <ola@okelola.com>
2022-12-11 22:25:36 -0800

    day 12 part 2

--- day12.go
+++ day12.go
@@ -17,107 +17,144 @@ func day12() {
 
 	// g.Print(func(val interface{}) string {
 	// 	return string(val.(rune))
-	// 	// return fmt.Sprintf("%s", val)
 	// })
-	// return
 
 	makeKey := func(i, j int) string {
 		return fmt.Sprintf("%d-%d", i, j)
 	}
 
-	q := make(map[string]bool)
-	mins := make(map[string]int)
-	unvisitedmins := make(map[string]bool)
+	elevations := map[rune]int{
+		'S': 1,
+	}
+	for c := 'a'; c <= 'z'; c++ {
+		elevations[c] = int(c-'a') + 1
+	}
 
-	var startPos *grid.Pos
+	var q map[string]bool
+	var mins map[string]int
+	var unvisitedmins map[string]bool
+
+	initIsh := func() {
+
+		q = make(map[string]bool)
+		mins = make(map[string]int)
+		unvisitedmins = make(map[string]bool)
+
+		for i := 0; i < g.XLength; i++ {
+			for j := 0; j < g.YLength; j++ {
+				k := makeKey(i, j)
+				q[k] = true
+				g.At(i, j).Visited = false
+			}
+		}
+	}
+
+	var startPoses []*grid.Pos
 	var endPos *grid.Pos
 	for i := 0; i < g.XLength; i++ {
 		for j := 0; j < g.YLength; j++ {
-			k := makeKey(i, j)
-			q[k] = true
-
 			curr := g.At(i, j).Rune()
-			if curr == 'S' {
-				startPos = &grid.Pos{Row: i, Column: j}
+			if curr == 'S' || curr == 'a' {
+				startPoses = append(startPoses, &grid.Pos{Row: i, Column: j})
 			}
 			if curr == 'E' {
 				endPos = &grid.Pos{Row: i, Column: j}
 			}
 		}
 	}
+	fmt.Println("total starts", len(startPoses))
 
-	// set initial
-	mins[makeKey(startPos.Row, startPos.Column)] = 0
+	min := math.MaxInt
 
-	currPos := startPos
-	for len(q) > 0 {
-		neighbors := g.Neighbors(currPos.Row, currPos.Column)
-		currRune := g.At(currPos.Row, currPos.Column).Rune()
+	for _, startPos := range startPoses {
+		initIsh()
+		// fmt.Println("startPos", startPos)
 
-		// there should be something if we're visiting it...
-		key := makeKey(currPos.Row, currPos.Column)
-		currVal := mins[key]
+		// set initial
+		mins[makeKey(startPos.Row, startPos.Column)] = 0
 
-		for _, v := range neighbors {
-			neigh := g.At(v.Row, v.Column)
-			if neigh.Visited {
-				continue
-			}
-			neighRune := g.At(v.Row, v.Column).Rune()
-			if neighRune == 'E' && currRune != 'z' {
-				continue
-			}
-			if currRune != 'S' {
-				if neighRune-currRune > 1 {
+		currPos := startPos
+		for len(q) > 0 {
+			neighbors := g.Neighbors(currPos.Row, currPos.Column)
+			currRune := g.At(currPos.Row, currPos.Column).Rune()
+
+			// there should be something if we're visiting it...
+			key := makeKey(currPos.Row, currPos.Column)
+			currVal := mins[key]
+
+			for _, v := range neighbors {
+				neigh := g.At(v.Row, v.Column)
+				neighRune := g.At(v.Row, v.Column).Rune()
+
+				if neighRune == 'E' && currRune != 'z' {
 					continue
+				}
+				if neigh.Visited {
+					// fmt.Println("visited continue", currPos, v.Row, v.Column, string(currRune), string(neighRune))
+					continue
+				}
+
+				currElevation := elevations[currRune]
+				neighElevation := elevations[neighRune]
+				if neighElevation-currElevation > 1 {
+					// fmt.Println("elevation continue", currPos, v.Row, v.Column, string(currRune), string(neighRune))
+
+					continue
+				}
+
+				newMin := currVal + 1
+
+				neighKey := makeKey(v.Row, v.Column)
+				neighMin := mins[neighKey]
+
+				if neighMin == 0 || newMin < neighMin {
+					mins[neighKey] = newMin
+					unvisitedmins[neighKey] = true
 				}
 			}
 
-			newMin := currVal + 1
+			// mark visited
+			delete(q, key)
+			delete(unvisitedmins, key)
+			g.At(currPos.Row, currPos.Column).Visited = true
 
-			neighKey := makeKey(v.Row, v.Column)
-			neighMin := mins[neighKey]
-
-			if neighMin == 0 || newMin < neighMin {
-				mins[neighKey] = newMin
-				unvisitedmins[neighKey] = true
+			if currRune == 'E' {
+				break
 			}
-		}
 
-		// mark visited
-		delete(q, key)
-		delete(unvisitedmins, key)
-		g.At(currPos.Row, currPos.Column).Visited = true
+			var newCurrPos *grid.Pos
+			min := math.MaxInt
+			for k := range unvisitedmins {
+				v := mins[k]
+				if v == 0 {
+					continue
+				}
+				parts := strings.Split(k, "-")
+				r := atoi(parts[0])
+				c := atoi(parts[1])
+				if g.At(r, c).Visited {
+					continue
+				}
 
-		if currRune == 'E' {
-			break
-		}
-
-		var newCurrPos *grid.Pos
-		min := math.MaxInt
-		for k := range unvisitedmins {
-			v := mins[k]
-			if v == 0 {
+				if v < min {
+					min = v
+					newCurrPos = &grid.Pos{Row: r, Column: c}
+				}
+			}
+			if newCurrPos != nil {
+				currPos = newCurrPos
 				continue
-			}
-			parts := strings.Split(k, "-")
-			r := atoi(parts[0])
-			c := atoi(parts[1])
-			if g.At(r, c).Visited {
-				continue
-			}
-
-			if v < min {
-				min = v
-				newCurrPos = &grid.Pos{Row: r, Column: c}
+			} else {
+				// why is this needed?
+				break
 			}
 		}
-		if newCurrPos != nil {
-			currPos = newCurrPos
-			continue
+		lastKey := makeKey(endPos.Row, endPos.Column)
+		// cheating?
+		if mins[lastKey] < min && mins[lastKey] != 0 {
+			min = mins[lastKey]
 		}
 	}
 
-	lastKey := makeKey(endPos.Row, endPos.Column)
-	fmt.Println(mins[lastKey])
+	fmt.Println("answer", min)
 }
