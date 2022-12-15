c3715e575db164f762a96ea781bf52a5698290b6
Ola Okelola <ola@okelola.com>
2022-12-15 12:26:42 -0800

    day 15 alternate -> day 15 +fixes cleanup

--- day15.go
+++ day15.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"sort"
 	"strings"
 
 	"github.com/lolopinto/adventofcode2020/grid"
@@ -21,13 +23,10 @@ func day15() {
 		return abs(p1.Row, p2.Row) + abs(p1.Column, p2.Column)
 	}
 
-	// missing
 	where := make(map[grid.Pos]rune)
-	dist := make(map[grid.Pos]int)
-	missing := make(map[grid.Pos]rune)
 
-	rows := []int{}
 	sensors := []grid.Pos{}
+	beacons := []grid.Pos{}
 
 	for _, line := range lines {
 		parts := strings.Split(line, " ")
@@ -36,59 +35,172 @@ func day15() {
 		where[sensor] = 'S'
 		where[beacon] = 'B'
 
-		dist[sensor] = mandistance(beacon, sensor)
-
-		rows = append(rows, sensor.Row, beacon.Row)
-
 		sensors = append(sensors, sensor)
+		beacons = append(beacons, beacon)
 	}
 
-	c := 10
-	for r := min(rows); r <= max(rows); r++ {
-		// for c := min(cols); c <= max(cols); c++ {
+	type missingRange struct {
+		// just rows
+		start, end int
+	}
 
-		log := false
-		if r == -2 || r == 14 || r == 24 {
-			log = true
+	ranges := map[int][]missingRange{}
+
+	miny := 0
+
+	// example
+	// searchy := 10
+	// maxy := 20
+
+	// question
+	searchy := 2000000
+	maxy := 4000000
+
+	// does a contain b
+	contains := func(a, b missingRange) bool {
+		return a.start <= b.start &&
+			b.end <= a.end
+	}
+
+	intersects := func(a, b missingRange) bool {
+		return a.start <= b.start &&
+			b.start <= a.end
+	}
+
+	maybeAddRange := func(currentCol int, r missingRange) {
+
+		currentRanges := ranges[currentCol]
+		if currentRanges == nil {
+			currentRanges = []missingRange{}
 		}
-		pos := grid.NewPos(r, c)
-		_, ok := where[pos]
-		// sensor or beacon, nothing to do here
-		if ok {
-			// if log {
-			// 	fmt.Printf("something %v at %v \n", v, pos)
-			// }
-			// fmt.Println("existing", pos)
+		for idx, existing := range currentRanges {
+
+			if contains(existing, r) {
+				// drop new one
+				return
+			}
+
+			if contains(r, existing) {
+				// update existing
+				currentRanges[idx] = r
+				ranges[currentCol] = currentRanges
+				return
+			}
+
+			// intersects + r bigger
+			if intersects(existing, r) {
+
+				// extend the back
+				currentRanges[idx] = missingRange{
+					start: existing.start,
+					end:   r.end,
+				}
+				ranges[currentCol] = currentRanges
+				return
+			}
+
+			// intersects + existing bigger
+			if intersects(r, existing) {
+
+				// extend the back
+				currentRanges[idx] = missingRange{
+					start: r.start,
+					end:   existing.end,
+				}
+				ranges[currentCol] = currentRanges
+				return
+			}
+		}
+
+		currentRanges = append(currentRanges, r)
+		ranges[currentCol] = currentRanges
+	}
+
+	sum := 0
+
+	for i := 0; i < len(sensors); i++ {
+		sensor := sensors[i]
+		beacon := beacons[i]
+
+		dist := mandistance(sensor, beacon)
+
+		for i := 0; i <= dist; i++ {
+			delta := dist - i
+
+			cols := []int{sensor.Column + i, sensor.Column, sensor.Column - i}
+
+			for _, col := range cols {
+				maybeAddRange(
+					col,
+					missingRange{
+						start: sensor.Row - delta,
+						end:   sensor.Row + delta,
+					})
+			}
+		}
+	}
+
+	// fix any duplicate ranges so that we end up with something easily parseable
+	// sort ranges and remove ranges already contained in sorted version
+
+	for k, potentialranges := range ranges {
+
+		sort.Slice(potentialranges, func(i, j int) bool {
+			return potentialranges[i].start < potentialranges[j].start
+		})
+
+		var result []missingRange
+		for _, v := range potentialranges {
+			add := true
+			for _, safe := range result {
+				if contains(safe, v) {
+					add = false
+					break
+				}
+			}
+			if add {
+				result = append(result, v)
+			}
+		}
+		ranges[k] = result
+	}
+
+	result := ranges[searchy]
+
+	lastend := -1
+	// sum up all missing beacons based on ranges
+	for i, v := range result {
+		sum += (v.end - v.start) + 1
+
+		if i != 0 && lastend > v.start {
+			sum -= (lastend + 1 - v.start)
+		}
+		lastend = v.end
+	}
+
+	// subtract locations that have beacons or sensors
+	for v := range where {
+		if v.Column == searchy {
+			sum--
+		}
+	}
+
+	fmt.Println("part 1:", sum)
+
+	for col, cands := range ranges {
+		if col > maxy || col < miny || len(cands) == 1 {
 			continue
 		}
-		for _, sensor := range sensors {
-			newdist := mandistance(pos, sensor)
-			// tie logic means something else is broken
-			// There is never a tie where two beacons are the same distance to a sensor.
-			if newdist < dist[sensor] {
-				// fmt.Println(pos)
-				missing[pos] = '#'
-			} else if log {
-				fmt.Printf("conflict pos %v sensor %v newdist %d existing dist %d \n", pos, sensor, newdist, dist[sensor])
+		lastend := -1
+		// search for the missing beacon
+		for i, c := range cands {
+			if i != 0 {
+				if c.start-lastend > 1 {
+					fmt.Println("part 2:", (lastend+1)*4000000+col)
+					os.Exit(0)
+				}
 			}
-			// else if log {
-			// 	// fmt.Printf("distance fail %d pos %v mandist %d \n", newdist, pos, dist[sensor])
-			// }
+			lastend = c.end
 		}
-		// }
 	}
-
-	// // fmt.Println(len(missing))
-	colct := map[int]int{}
-	for k := range missing {
-		ct := colct[k.Column]
-		ct++
-		colct[k.Column] = ct
-	}
-	// // fmt.Println(len(missing))
-	// fmt.Println(colct)
-	// // fmt.Println(where)
-	// fmt.Println(len(colct))
-	fmt.Println(colct[c])
-	// fmt.Println(ct)
 }
