caedc34e69d21f0c33789f599c1e66608946099a
Ola Okelola <ola@okelola.com>
2023-12-12 22:14:26 +0900

    day 12 part 2

--- 2023/day12.py
+++ 2023/day12.py
@@ -5,61 +5,64 @@ from collections import defaultdict
 from dataclasses import dataclass
 import re
 import itertools
+from functools import cache
 
-def replace_chars(s: str):
-  # chat gpt helped write this :)
-  char_to_replace = '?'
-  replacements = ['#', '.']
-  positions = [i for i, c in enumerate(s) if c == char_to_replace]
-  combos = itertools.product(replacements, repeat=len(positions))
-
-  result = []  
-  for combo in combos:
-    new_s = list(s)
-    for pos, replacement in zip(positions, combo):
-      new_s[pos] = replacement
-    yield ''.join(new_s)
-  return result
-
-def is_valid(s: str, damaged: list[int]) -> bool:
-  cts = []
+# changed from all combinations to something that calculates as we're going through
+# inspired by something on reddit
+@cache
+def count_solutions(s: str, damaged: tuple[int], num_in_group: int) -> int:
+  if len(s) == 0:
+    return not damaged and not num_in_group
+  
   ct = 0
-  for c in s:
+  
+  possibilities = [s[0]]
+  if s[0] == '?':
+    possibilities = ['.', '#']
+  
+  for c in possibilities:
     match c:
       case '#':
-        ct += 1
-      case '.':
-        if ct > 0:
-          cts.append(ct)
-        ct = 0
+        ct += count_solutions(s[1:], damaged, num_in_group + 1)
       case _:
-        assert False
-  if ct > 0:
-    cts.append(ct)
-
-  return cts == damaged
+        if num_in_group and damaged and damaged[0] == num_in_group:
+            ct += count_solutions(s[1:], damaged[1:], 0)
+        elif not num_in_group:
+          ct += count_solutions(s[1:], damaged, 0)
+  return ct
 
 async def part1():
   valids = []
   async for line in read_file("day12input"):
     parts = line.split()
     assert len(parts) == 2
-    damaged = [int(v) for v in parts[1].split(",")]
-    springs = parts[0]
-    replacements = replace_chars(springs)
-    valid = sum([1 for replacement in replacements if is_valid(replacement, damaged)])
-    valids.append(valid)
+    damaged = tuple([int(v) for v in parts[1].split(",")])
+    ct = count_solutions(parts[0] + "_", damaged, 0)
+    valids.append(ct)
 
   print(sum(valids))
 
 
+def transform_line(line: str) -> str:
+  parts = line.split()
+  assert len(parts) == 2
+  damaged = [int(v) for v in parts[1].split(",")]
+  springs = '?'.join([parts[0]] * 5)
+  damages = ",".join([parts[1]] * 5)
+  return f"{springs} {damages}"
+
+
 async def part2():
+  valids = []
   async for line in read_file("day12input"):
-    pass
+    line = transform_line(line)
+    parts = line.split()
+    assert len(parts) == 2
+    damaged = tuple([int(v) for v in parts[1].split(",")])
+    ct = count_solutions(parts[0] + "_", damaged, 0)
+    valids.append(ct)
 
-
-
-
+  print(sum(valids))
 
 
 if __name__ == "__main__":
