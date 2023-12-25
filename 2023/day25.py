855f84a52086a5c02c47964d006ae2cd9092bef2
Ola Okelola <ola@okelola.com>
2023-12-25 10:48:45 -0800

    day25 use networkx instead of trying to solve manually

--- 2023/day25.py
+++ 2023/day25.py
@@ -9,96 +9,27 @@ from grid import Grid
 import itertools
 import enum
 import math
-import sys
-from functools import cache
-from copy import deepcopy
-sys.setrecursionlimit(10000)
+import networkx as nx
 
 @dataclass
 class Node:
   name: str
   linked: set[str]
 
-def connected_groups(node_keys: Iterable[str], nodes: dict[str, Node], combos: Iterable[str]) -> list[set[str]]:
-  
-  @cache
-  def connected_groups_helper(node_keys: Iterable[str], combos: Iterable[str], ) -> list[set[str]]:
-    connected = set()
-    disconnected = set()
-
-    keys = list(node_keys)
-    first = keys[0]
-    
-    to_process = [first]
-    while len(to_process) > 0:
-      node = to_process.pop()
-      connected.add(node)
-      for key in nodes[node].linked:
-        if key not in connected:
-          to_process.append(key)
-
-    disconnected = tuple(set(keys) - connected)
-
-    return [connected, disconnected]        
-
-  return connected_groups_helper(node_keys, combos)
-
 async def part1():
-  nodes = {}
+  # got this from someone in reddit
+  # need to start using all these tools that all these folks use instead of reinventing the wheel
+  G = nx.Graph()
 
   async for line in read_file("day25input"):
     left, right = line.split(": ")
-    if left not in nodes:
-      nodes[left] = Node(left, set())
     for right in right.split(" "):
-      if right not in nodes:
-        nodes[right] = Node(right, set())
-      nodes[right].linked.add(left)
-      nodes[left].linked.add(right)
+      G.add_edge(left, right)
 
-  node_keys = tuple(nodes.keys())
-  for combos in itertools.combinations(node_keys, 6):
+  G.remove_edges_from(nx.minimum_edge_cut(G))
 
-    valid = set()
-    unique_items = set()
-
-    for combo in itertools.combinations(combos, 2):
-
-        l, r = combo
-        left = nodes[l]
-        right = nodes[r]
-
-        if l in right.linked and r in left.linked:
-          valid.add(combo)
-          unique_items.add(l)
-          unique_items.add(r)
-
-    if len(valid) != 3 and len(unique_items) != 6:
-      continue
-    
-    # need one more combination of these ones to get the valid thing
-    # seems like too many combinations
-    for valid in itertools.combinations(valid, 3):
-
-      nodes2 = deepcopy(nodes)
-      for group in valid:
-        l, r = group
-        nodes2[l].linked.remove(r)
-        nodes2[r].linked.remove(l)
-        
-      # now go through and group them
-      ret = connected_groups(node_keys, nodes2, node_keys)
-    
-      if len(ret) == 2 and len(ret[1]) != 0:
-        print(len(ret[0]) * len(ret[1]))
-        return
-
-
-async def part2():
-  async for line in read_file("day3input"):
-    pass
+  print(math.prod([len(c) for c in nx.connected_components(G)]))
 
 
 if __name__ == "__main__":
     asyncio.run(part1())
-    asyncio.run(part2())
