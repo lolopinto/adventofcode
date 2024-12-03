ae746c991d32d36a35559ce3e07ac69b96e167e7
Ola Okelola <ola@okelola.com>
2024-12-02 23:24:18 -0800

    day 3 part 2

--- 2024/day3.py
+++ 2024/day3.py
@@ -19,10 +19,40 @@ async def part1():
 
 
 async def part2():
+  s = 0
+  enabled = True
+  first = True
   async for line in read_file("day3input"):
-    pass
-
+    dont_matches = [v.start() for v in re.finditer("don't()", line)]
+    do_matches = [v.start() for v in re.finditer("do\(\)", line)]
+    
+    l = []
+    for v in dont_matches:
+      l.append((v, False))
+    for v in do_matches:  
+      l.append((v, True))
+    for item in re.finditer("mul\(\d+,\d+\)", line):
+      l.append((item.start(), item))
+    
+    l.sort(key=lambda x: x[0])    
+    if first:
+      l.insert(0, (-1, True))
+      first = False
+    
+    for _, item in l:
+      if item is True:
+        enabled = True
+        continue
+      if item is False:
+        enabled = False
+        continue
+      if not enabled:
+        continue
+      v = re.findall("mul\((\d+,\d+)\)", item.group())[0].split(",")
+      s += int(v[0]) * int(v[1])
+      
+  print(s)
 
 if __name__ == "__main__":
     asyncio.run(part1())
-    # asyncio.run(part2())
+    asyncio.run(part2())
