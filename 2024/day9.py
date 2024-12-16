a664b783d0ff7abb473ab269b6e54bf43cb70e97
Ola Okelola <ola@okelola.com>
2024-12-16 17:01:04 +0900

    day 9 part 2

--- 2024/day9.py
+++ 2024/day9.py
@@ -9,14 +9,16 @@ import itertools
 import enum
 import math
 
-# @dataclass
-# class File:
-#   file_id: int
-#   blocks: int
 
-# @dataclass
-# class FreeSpace:
-#   amount: int
+@dataclass
+class File:
+  file_id: int
+  blocks: int
+
+@dataclass
+class FreeSpace:
+  amount: int
+ 
 
 async def part1():
   lines = [line async for line in read_file("day9input")]
@@ -27,20 +29,14 @@ async def part1():
   is_file = True
   
   l = []
-  free_space_indices = []
 
   for c in lines[0]:
     v = int(c)
     if is_file:
       l.extend([file_id] * v)
-      # items.append(File(blocks=v, file_id=file_id))
       file_id += 1
     else:
-      start_idx = len(l)
       l.extend(["."] * v)
-      for i in range(start_idx, v+start_idx):
-        free_space_indices.append(i)
-      # items.append(FreeSpace(amount=v))
       
     is_file = not is_file
   
@@ -64,8 +60,6 @@ async def part1():
       if free_idx == len(free_space_indices):
         break
     
-  swap(l, free_space_indices)
-
   while True:
     free_space_indices = []
     last_number_idx = -1
@@ -90,13 +84,131 @@ async def part1():
       s += v * idx
 
   print(s)
-  
+
 
 async def part2():
-  async for line in read_file("day9input"):
-    pass
+  lines = [line async for line in read_file("day9input")]
+  assert len(lines) == 1
+  
+  items = []
+  file_id = 0
+  is_file = True
+  last_file_id = None
+  
+  for c in lines[0]:
+    v = int(c)
+    if is_file:
+      items.append(File(blocks=v, file_id=file_id))
+      last_file_id = file_id
+      file_id += 1
+    elif v > 0:
+      items.append(FreeSpace(amount=v))
+    is_file = not is_file
+  
+  
+  def print_items():
+    line = ""
+    for item in items:
+      if isinstance(item, FreeSpace):
+        line += ("." * item.amount)
+      else:
+        line += (f"{item.file_id}" * item.blocks)
+    print(line)
+  
+  def swap(items, free_space_indices, file_indices) -> tuple[bool, list | None]:
+    nonlocal last_file_id
 
+    for file_idx in range(len(file_indices)-1, -1, -1):
 
+      i = file_indices[file_idx]
+      item = items[i]
+      if isinstance(item, FreeSpace):
+        continue
+      
+      if item.file_id > last_file_id:
+        continue
+      
+      last_file_id = item.file_id
+
+      for free_idx in range(len(free_space_indices)):
+
+        
+        item_idx_swap = free_space_indices[free_idx]
+        if item_idx_swap >= i:
+          break
+        
+        free_space = items[item_idx_swap]
+        
+        can_swap = free_space.amount >= item.blocks
+
+        if can_swap:
+
+          new_list = []
+
+          def add_to_list(item):
+            if not isinstance(item, FreeSpace) or len(new_list) == 0:
+              new_list.append(item)
+              return
+            
+            last = new_list[-1]
+            if not isinstance(last, FreeSpace):
+              new_list.append(item)
+              return
+            
+            last.amount += item.amount
+            
+          for ii, v in enumerate(items):
+            
+            if ii == i:
+              # add free space in new replacement spot
+              add_to_list(FreeSpace(amount=item.blocks))
+              continue
+
+            if ii != item_idx_swap:
+              add_to_list(v)
+              continue
+            
+            add_to_list(item)
+            if free_space.amount > item.blocks:
+              add_to_list(FreeSpace(amount=free_space.amount - item.blocks))
+              
+          return True, new_list
+
+    return False, None
+  
+  while True:
+    free_space_indices = []
+    file_indices = []
+    
+    for idx, v in enumerate(items):
+      if isinstance(v, FreeSpace):
+        free_space_indices.append(idx)
+      else:
+        file_indices.append(idx)
+
+    swapped, new_l = swap(items, free_space_indices, file_indices)
+    # print()
+    # print(swapped, new_l)
+    
+    if not swapped:
+      break
+    
+    items = new_l
+
+  idx = 0
+  ss = 0
+  for item in items:
+    if isinstance(item, FreeSpace):
+      idx += item.amount
+      continue
+    
+    for j in range(item.blocks):
+      ss += (item.file_id * idx)
+      idx += 1
+    
+  print(ss)
+    
+  
 if __name__ == "__main__":
     asyncio.run(part1())
     asyncio.run(part2())
