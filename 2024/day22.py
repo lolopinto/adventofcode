9978fd22ff50f832085cfcf395d33a036431de20
Ola Okelola <ola@okelola.com>
2024-12-22 17:06:38 +0800

    day 22 part 2

--- 2024/day22.py
+++ 2024/day22.py
@@ -29,9 +29,16 @@ def next_secret(secret: int) -> int:
   return secret
 
 def get_nth_secret(secret: int, n: int) -> int:
+  last = None
+  for secret in yield_nth_secret(secret, n):
+    last = secret
+  assert last is not None
+  return last
+
+def yield_nth_secret(secret: int, n: int) -> int:
   for i in range(n):
     secret = next_secret(secret)
-  return secret
+    yield secret
 
 async def part1():
   s = 0
@@ -41,10 +48,47 @@ async def part1():
   print(s)
 
 
-async def part2():
-  async for line in read_file("day3input"):
-    pass
+def get_deltas(secret: int, n: int) -> dict[tuple[int, int, int, int], int]: 
+  last = secret % 10
+  deltas = []
+  
+  m = dict()
+  for secret in yield_nth_secret(secret, n):
+    curr = secret % 10
+    delta = curr - last
+    last = curr
+    deltas.append(delta)
+    if len(deltas) >= 4:
+      t = tuple(deltas[-4:])
+      if t not in m:
+        m[t] = curr
+      # print(t, curr)
 
+  return m
+
+async def part2():
+  deltas_map = dict()
+  all_deltas = set()
+  all_numbers = []
+  async for line in read_file("day22input"):
+    n = int(line)
+    deltas = get_deltas(n, 2000)
+    deltas_map[n] = deltas
+    all_deltas.update(deltas.keys())
+    all_numbers.append(n)
+    
+
+  curr_max = 0      
+  for delta in all_deltas:
+    s = 0
+    for n in all_numbers:
+      if delta in deltas_map[n]:
+        s += deltas_map[n][delta]
+
+    curr_max = max(curr_max, s)
+    
+  print(curr_max)
+    
 
 if __name__ == "__main__":
     asyncio.run(part1())
